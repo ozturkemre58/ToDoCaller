001d90abd032383230a8b265085abd3f49615a5c
ozturkemre58 <emreozturk.5874@gmail.com>
2024-02-05 00:02:20 +0300

    First Commit

---
+++ ToDoCaller/ToDoCallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoCallerApp.swift
+//  ToDoCaller
+//
+//  Created by Emre Öztürk on 4.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoCallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
