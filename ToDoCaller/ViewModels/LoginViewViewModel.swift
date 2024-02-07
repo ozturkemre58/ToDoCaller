21b0565fa00d063b3e5264d374dc44e86d4447b1
ozturkemre58 <emreozturk.5874@gmail.com>
2024-02-07 18:30:38 +0300

    login viewmodel added

---
+++ ToDoCaller/ViewModels/LoginViewViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewViewModel.swift
+//  ToDoCaller
+//
+//  Created by Emre Öztürk on 6.02.2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+class LoginViewViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = "sas"
+    init() {}
+    
+    func login() {
+        guard validate() else {
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password)
+            
+    }
+    
+    func validate() -> Bool {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty, !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessage = "Please fill in all fields"
+            return false
+        }
+     
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter valid email."
+            return false
+        }
+        
+        
+        return true
+    }
+}
+
