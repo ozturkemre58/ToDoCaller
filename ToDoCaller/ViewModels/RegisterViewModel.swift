fb08f62180b43bb2c2c4a1c1ea734114d6c568f7
ozturkemre58 <emreozturk.5874@gmail.com>
2024-02-09 00:09:40 +0300

    maim viewmodel register view model edited

--- ToDoCaller/ViewModels/RegisterViewModel.swift
+++ ToDoCaller/ViewModels/RegisterViewModel.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+class RegisterViewModel: ObservableObject {
+    
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    init() {}
+    
+    func registerAction() {
+        guard validate() else {
+            return
+        }
+        
+         Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard let userId = result?.user.uid else {
+                return
+            }
+            
+            self?.insertUserRecord(userId: userId)
+        }
+    }
+    
+    private func insertUserRecord(userId: String) {
+        let newUser = User(id: userId, name: name, email: email, joined: Date().timeIntervalSince1970)
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId)
+            .setData(newUser.asDictionary())
+    }
+    
+    func validate() -> Bool {
+        guard name.trimmingCharacters(in: .whitespaces).isEmpty,
+              email.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return true
+        }
+        
+        guard email.contains("@") && email.contains(".") else {
+            return true
+        }
+        
+        guard password.count >= 6 else {
+            return true
+        }
+        
+        return true
+    }
+    
+}
