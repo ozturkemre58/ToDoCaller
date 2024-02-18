5780c97e59882aedf2ea92b18ef7aa5521dfe4be
ozturkemre58 <emreozturk.5874@gmail.com>
2024-02-18 03:11:33 +0300

    new ıtem view added

--- ToDoCaller/ViewModels/NewItemViewViewModel.swift
+++ ToDoCaller/ViewModels/NewItemViewViewModel.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+class NewItemViewViewModel: ObservableObject {
+    @Published var title = ""
+    @Published var dueDate = Date()
+    @Published var showAlert = false
+    
+    init() {}
+    
+    func save() {
+        guard canSave else {
+            return
+        }
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false)
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+    }
+    
+    var canSave: Bool {
+        guard !title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else {
+            return false
+        }
+        
+        guard dueDate >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        
+        return true
+    }
+}
--- ToDoCaller/Views/NewItemView.swift
+++ ToDoCaller/Views/NewItemView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct NewItemView: View {
+    
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("New Item")
+                .font(.system(size: 32))
+                .bold()
+                .padding(.top, 20)
+            
+            Form {
+                
+                TextField("Title", text: $viewModel.title)
+                    .textFieldStyle(DefaultTextFieldStyle())
+        
+                DatePicker("Due Date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .tint(.green)
+                
+                TLButton(title: "Save", bgColor: .green) {
+                    if viewModel.canSave {
+                        viewModel.save()
+                        newItemPresented = false
+                    } else {
+                        viewModel.showAlert = true
+                    }
+                }
+                .padding()
+            }
+            .alert(isPresented: $viewModel.showAlert, content: {
+            Alert(title: Text("Error"), message: Text("Please Fill in all fields and select due date that is today or never."))
+            })
+        }
     }
 }
 
+
 #Preview {
-    NewItemView()
+    NewItemView(newItemPresented: Binding(get: {
+        return true
+    }, set: { _ in
+    }))
 }
