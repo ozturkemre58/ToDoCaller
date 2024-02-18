dea271d2d375fc297cff80750eb348fc42f8ee1e
ozturkemre58 <emreozturk.5874@gmail.com>
2024-02-19 01:33:34 +0300

    Delete func added

--- ToDoCaller/Views/ToDoListView.swift
+++ ToDoCaller/Views/ToDoListView.swift
@@ -1,52 +1,44 @@
-//
-//  ToDoListView.swift
-//  ToDoCaller
-//
-//  Created by Emre Öztürk on 5.02.2024.
-//
-
 import SwiftUI
 import FirebaseFirestoreSwift
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
-       @FirestoreQuery var items: [ToDoListItem]
-       private let userId: String
-       
-       init(userId: String) {
-           self.userId = userId
-           self._items = FirestoreQuery(
-               collectionPath: "users/\(userId)/todos"
-           )
-       }
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos"
+        )
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
+    }
     
     var body: some View {
-        
         NavigationView {
             VStack {
                 List(items) { item in
                     ToDoListItemsView(item: item)
                         .swipeActions {
                             Button("Delete") {
-                                viewModel.deleteToDo(item.id)
+                                self.viewModel.deleteToDo(item.id)
                             }
-                        }
-                           
-
+                        }.tint(.red)
                 }
                 .listStyle(PlainListStyle())
-            }.padding()
-            .navigationTitle("ToDos")
-            .toolbar {
-                Button {
-                    viewModel.showingNewItemView = true
-                } label: {
-                    Image(systemName: "plus")
+                .padding()
+                .navigationTitle("ToDos")
+                .toolbar {
+                    Button {
+                        viewModel.showingNewItemView = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
                 }
+                .sheet(isPresented: $viewModel.showingNewItemView, content: {
+                    NewItemView(newItemPresented: $viewModel.showingNewItemView)
+                })
             }
-            .sheet(isPresented: $viewModel.showingNewItemView, content: {
-                NewItemView(newItemPresented: $viewModel.showingNewItemView)
-            })
         }
     }
 }
