ec53d1305e2767eb4a6c1343207f56f5a8ab32e9
ozturkemre58 <emreozturk.5874@gmail.com>
2024-02-19 03:13:05 +0300

    Profile View added Profile view view model fetcUser func added

--- ToDoCaller/ViewModels/ProfileViewViewModel.swift
+++ ToDoCaller/ViewModels/ProfileViewViewModel.swift
@@ -11,22 +11,32 @@ import FirebaseFirestore
 
 
 class ProfileViewViewModel: ObservableObject {
-    init() {}
- 
+    @Published var user: User? = nil
     
-    func toggleIsDone(item: ToDoListItem) {
-        var itemCopy = item
-        itemCopy.setDone(!item.isDone)
-        guard let uid = Auth.auth().currentUser?.uid else {
+    init() {}
+    
+    func fetchUser() {
+        guard let userId = Auth.auth().currentUser?.uid else {
             return
         }
         
         let db = Firestore.firestore()
+        db.collection("users").document(userId).getDocument { [weak self] snapshot, error in
+            guard let data = snapshot?.data(), error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self?.user = User(
+                    id: data["id"] as? String ?? "",
+                    name: data["name"] as? String ?? "",
+                    email: data["email"] as? String ?? "",
+                    joined: data["joined"] as? TimeInterval ?? 0)
+            }
+        }
+    }
+    
+    func logOut() {
         
-        db.collection("users")
-            .document(uid)
-            .collection("todos")
-            .document(itemCopy.id)
-            .setData(itemCopy.asDictionary())
     }
 }
--- ToDoCaller/Views/ProfileView.swift
+++ ToDoCaller/Views/ProfileView.swift
@@ -14,10 +14,55 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                   profile(user: user)
+                } else {
+                    Text("Loading Profile...")
+                }
             }
-            .navigationTitle("Profile")
+            .padding(.top, 50)
+            .onAppear {
+                viewModel.fetchUser()
+            }
+            
+            
         }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        //Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.green)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }.padding()
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }.padding()
+            
+            Spacer()
+            
+            HStack {
+                Spacer()
+                TLButton(title: "Log Out", bgColor: .green) {
+                    viewModel.logOut()
+                }
+                .frame(width: 150, height: 75)
+                Spacer()
+            }.padding(.bottom, 20)
+        }
+        .padding(.top, 50)
     }
 }
 
